3c960f979976aa3fa73b4d9f3d88116f92585661
Angelika Plonka <angelikaplonka89@gmail.com>
2018-05-20 09:33:29 +0200

    created a connection with database and added an entity User

---
+++ src/Entity/User.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="string", length=2)
+     */
+    private $country_code;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $identification_number;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getCountryCode(): ?string
+    {
+        return $this->country_code;
+    }
+
+    public function setCountryCode(string $country_code): self
+    {
+        $this->country_code = $country_code;
+
+        return $this;
+    }
+
+    public function getIdentificationNumber(): ?int
+    {
+        return $this->identification_number;
+    }
+
+    public function setIdentificationNumber(int $identification_number): self
+    {
+        $this->identification_number = $identification_number;
+
+        return $this;
+    }
+}
